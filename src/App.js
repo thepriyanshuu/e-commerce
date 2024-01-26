351e0c860c99f4e7dee3952a6d33c874a3bb383f
thepriyanshuu <pranshu020304@gmail.com>
2024-01-26 16:04:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-commerce</h1>
+      <button>Normal button</button>
+      <button>Bootstrap button</button>
     </div>
   );
 }
